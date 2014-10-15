0131297b79035d296a30d2075d883c6b23fab824
kezhizhi <kezhizhi@163.com>
2014-10-15 16:52:56 +0800

    update

--- doc/demo/day1/test/app.js
+++ doc/demo/day1/test/app.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+//自定义路由
+var myHttpGet = require('./routes/getserver');
+
 var app = express();
 
 // view engine setup
@@ -15,15 +18,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/users', users);
+app.use('/getserver', myHttpGet);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
