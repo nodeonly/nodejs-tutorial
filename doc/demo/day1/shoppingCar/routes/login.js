b0660644bfedfc08b37bb05bd39223727d46f868
xiaolongXL <rainbolean@sina.com>
2014-10-16 18:21:49 +0800

    update document

--- doc/demo/day1/shoppingCar/routes/login.js
+++ doc/demo/day1/shoppingCar/routes/login.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res) {
+	if(req.session.name){
+		console.log('yes');
+	}else{
+		console.log('no');
+	};
  	res.send({
  		code:'0'
  	});
--- doc/demo/day1/shoppingCar/routes/register.js
+++ doc/demo/day1/shoppingCar/routes/register.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res) {
+	var reqJson = req.body;
+	req.session.name = reqJson.name; 
  	res.send({
- 		code:'0'
+ 		code:0
  	});
 });
 
