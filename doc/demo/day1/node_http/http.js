ab78ba9b13b7e048933ae4beb7b0ec2e57ef8026
alfred sang <i5ting@126.com>
2014-10-16 12:15:59 +0800

    add session part

--- doc/demo/day1/node_http/http.js
+++ doc/demo/day1/node_http/http.js
@@ -2,5 +2,7 @@ var http = require('http');
  
 http.createServer(function(request,response){
     console.log(request);
+	// console.log(request.url);
+	// console.log(request.headers['user-agent']);
     response.end('Hello world!');
 }).listen(8888);
